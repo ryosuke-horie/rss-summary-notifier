d4ccd322a64249ce4e9f3884fdf2b3a3b5c05181
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-06-13 11:21:47 +0900

    update

--- lib/rss-summary-notifier-stack.ts
+++ lib/rss-summary-notifier-stack.ts
@@ -151,7 +151,7 @@ export class RssSummaryNotifierStack extends Stack {
 			// 通知のスケジュールを取得
 			// 実行するのは → 毎日の0時
 			const schedule: CronOptions = notifier["schedule"] || {
-				minute: "22",
+				minute: "30",
 				hour: "*",
 				day: "*",
 				month: "*",
