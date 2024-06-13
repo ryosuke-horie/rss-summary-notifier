1a396c11d67b6055efe025248961ad83c5876a01
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-06-13 15:56:23 +0900

    OIDCスタックを定義

--- bin/rss-summary-notifier.ts
+++ bin/rss-summary-notifier.ts
@@ -3,6 +3,7 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { RssSummaryNotifierStack } from "../lib/rss-summary-notifier-stack";
 import { FrontendS3CloudFrontStack } from "../lib/frontend-s3-cloudfront-stack";
+import { OidcStack } from "../lib/oidc-stack";
 
 const app = new cdk.App();
 
@@ -16,6 +17,14 @@ new RssSummaryNotifierStack(app, "RssSummaryNotifierStack", {
 
 // CloudFront + S3でホスティング用のStackを作成する
 new FrontendS3CloudFrontStack(app, "FrontendS3CloudFrontStack", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
+});
+
+// GitHub Actions によるデプロイを許可する OIDC プロバイダーを作成する
+new OidcStack(app, "OidcStack", {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION,
