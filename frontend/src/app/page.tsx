f8e19bf79dd1d692b594af7ad9e76401e76d16f6
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-06-13 19:53:06 +0900

    DynamoDBからScanするAPIを実装

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import Card from "@/components/Card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-5xl font-bold text-center">Welcome to DaisyUI</h1>
-        <Card />
+      <h1 className="text-5xl font-bold text-center">Welcome to DaisyUI</h1>
+      <Card />
     </main>
   );
 }
