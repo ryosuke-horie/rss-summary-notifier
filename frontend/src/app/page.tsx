1adfa8cc9f224a57987efaf175124dc648516ead
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-06-13 20:02:07 +0900

    簡易実装

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,46 @@
+import { createDynamoDbClient } from "../lib/dynamodb";
+import { ScanCommand, type ScanCommandInput } from "@aws-sdk/lib-dynamodb";
+// import type { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import Card from "@/components/Card";
 
-export default function Home() {
+interface HomeProps {
+  items: {
+    title: string;
+    summary: string;
+    detail: string;
+    pubtime: string;
+    url: string;
+  }[];
+}
+
+const getData = async (): Promise<HomeProps> => {
+  const client: any = createDynamoDbClient();
+  const params: ScanCommandInput = {
+    TableName: process.env.TABLE_NAME,
+  };
+  const { Items: allItems } = await client.send(new ScanCommand(params));
+
+  const items = allItems.map((item: any) => ({
+    title: item.title,
+    summary: item.summary,
+    detail: item.detail,
+    pubtime: item.pubtime,
+    url: item.url,
+  }));
+
+  return { items };
+};
+
+export default async function Home() {
+  const { items } = await getData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-bold text-center">Welcome to DaisyUI</h1>
-      <Card />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {items.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
+      </div>
     </main>
   );
 }
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,4 +1,36 @@
-const Card = () => {
+// const Card = () => {
+//   return (
+//     <div className="card w-96 bg-base-100 shadow-xl">
+//       <figure>
+//         <img
+//           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+//           alt="Shoes"
+//         />
+//       </figure>
+//       <div className="card-body">
+//         <h2 className="card-title">Shoes!</h2>
+//         <p>If a dog chews shoes whose shoes does he choose?</p>
+//         <div className="card-actions justify-end">
+//           <button className="btn btn-primary">Buy Now</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+interface CardProps {
+  item: {
+    title: string;
+    summary: string;
+    detail: string;
+    pubtime: string;
+    url: string;
+  };
+}
+
+const Card: React.FC<CardProps> = ({ item }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -8,10 +40,12 @@ const Card = () => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{item.title}</h2>
+        <p>{item.summary}</p>
+        <p>{item.detail}</p>
+        <p>{new Date(item.pubtime).toLocaleString()}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">Read More</button>
         </div>
       </div>
     </div>
