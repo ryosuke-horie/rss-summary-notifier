ea3c3372cdffe2a59e63c02139e286b1ca1e8c88
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-06-15 12:09:16 +0900

    表示項目を削減し、余白を小さく設けた

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,48 +1,45 @@
 import { createDynamoDbClient } from "../lib/dynamodb";
 import { ScanCommand, type ScanCommandInput } from "@aws-sdk/lib-dynamodb";
-// import type { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import Card from "@/components/Card";
 
 interface HomeProps {
-  items: {
-    title: string;
-    summary: string;
-    detail: string;
-    pubtime: string;
-    url: string;
-    ogp_image: string;
-  }[];
+	items: {
+		title: string;
+		summary: string;
+		url: string;
+		ogp_image: string;
+	}[];
 }
 
 const getData = async (): Promise<HomeProps> => {
-  const client: any = createDynamoDbClient();
-  const params: ScanCommandInput = {
-    TableName: process.env.TABLE_NAME,
-  };
-  const { Items: allItems } = await client.send(new ScanCommand(params));
+	const client: any = createDynamoDbClient();
+	const params: ScanCommandInput = {
+		TableName: process.env.TABLE_NAME,
+	};
+	const { Items: allItems } = await client.send(new ScanCommand(params));
 
-  const items = allItems.map((item: any) => ({
-    title: item.title,
-    summary: item.summary,
-    detail: item.detail,
-    pubtime: item.pubtime,
-    url: item.url,
-    ogp_image: item.ogp_image,
-  }));
+	const items = allItems.map((item: any) => ({
+		title: item.title,
+		summary: item.summary,
+		url: item.url,
+		ogp_image: item.ogp_image,
+	}));
 
-  return { items };
+	return { items };
 };
 
 export default async function Home() {
-  const { items } = await getData();
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold text-center">Welcome to DaisyUI</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {items.map((item, index) => (
-          <Card key={index} item={item} />
-        ))}
-      </div>
-    </main>
-  );
+	const { items } = await getData();
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-12">
+			<h1 className="text-2xl font-bold text-left mb-8">
+				RSS Summary Notifiers
+			</h1>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
+				{items.map((item, index) => (
+					<Card key={index} item={item} />
+				))}
+			</div>
+		</main>
+	);
 }
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,39 +1,28 @@
-import React from 'react';
-
 interface CardProps {
-    item: {
-        title: string;
-        summary: string;
-        detail: string;
-        pubtime: string;
-        url: string;
-        ogp_image: string;
-    };
+	item: {
+		title: string;
+		summary: string;
+		url: string;
+		ogp_image: string;
+	};
 }
 
 const Card: React.FC<CardProps> = ({ item }) => {
-    const defaultImage = "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg";
-    const imageUrl = item.ogp_image ? item.ogp_image : defaultImage;
+	const defaultImage =
+		"https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg";
+	const imageUrl = item.ogp_image ? item.ogp_image : defaultImage;
 
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure>
-                <img
-                    src={imageUrl}
-                    alt="Thumbnail"
-                />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{item.title}</h2>
-                <p>{item.summary}</p>
-                <p>{item.detail}</p>
-                <p>{new Date(item.pubtime).toLocaleString()}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Read More</button>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="card w-96 bg-base-100 shadow-xl">
+			<figure>
+				<img src={imageUrl} alt="Thumbnail" />
+			</figure>
+			<div className="card-body">
+				<h2 className="card-title">{item.title}</h2>
+				<p>{item.summary}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
