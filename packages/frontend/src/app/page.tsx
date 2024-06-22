67bf4e32613227b53f7e76ee75a95d4afde594f9
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-06-22 22:05:43 +0900

    biome-ignore

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -92,6 +92,7 @@ export default async function Home() {
 						<h2 className="text-2xl font-bold mb-6">{category}</h2>
 						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
 							{items.map((item, index) => (
+								// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 								<Card key={index} item={item} />
 							))}
 						</div>
